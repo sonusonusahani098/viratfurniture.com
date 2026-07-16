51f416634b90feb7f8309b0913768b7d0405a9bf
Sonu Sahani <sonusahani086@gmail.com>
2026-07-16 22:43:05 +0530

    ui improve gallry

--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -359,11 +359,10 @@ const explorer = [
     <!-- Lightbox -->
     <Teleport to="body">
       <Transition
-        enter-active-class="transition duration-300 ease-out"
+        :duration="{ enter: 300, leave: 220 }"
+        enter-active-class="transition-opacity duration-300 ease-out"
         enter-from-class="opacity-0"
-        enter-to-class="opacity-100"
-        leave-active-class="transition duration-200 ease-in"
-        leave-from-class="opacity-100"
+        leave-active-class="transition-opacity duration-200 ease-in"
         leave-to-class="opacity-0"
       >
         <div
